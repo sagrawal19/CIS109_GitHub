60e8270a7e3fc1b1e644617989da289d73fff0df
Sibani Agrawal <sibani.agrawal@gmail.com>
2019-11-13 13:41:54 -0800

    Create week day label

--- DigitalClock/DigitalClock/Form1.Designer.cs
+++ DigitalClock/DigitalClock/Form1.Designer.cs
@@ -31,13 +31,14 @@
             this.hourMinuteLabel = new System.Windows.Forms.Label();
             this.secondLabel = new System.Windows.Forms.Label();
             this.dateLabel = new System.Windows.Forms.Label();
+            this.weekDayLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // hourMinuteLabel
             // 
             this.hourMinuteLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hourMinuteLabel.ForeColor = System.Drawing.Color.White;
-            this.hourMinuteLabel.Location = new System.Drawing.Point(127, 93);
+            this.hourMinuteLabel.Location = new System.Drawing.Point(127, 48);
             this.hourMinuteLabel.Name = "hourMinuteLabel";
             this.hourMinuteLabel.Size = new System.Drawing.Size(254, 94);
             this.hourMinuteLabel.TabIndex = 2;
@@ -47,7 +48,7 @@
             // 
             this.secondLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.secondLabel.ForeColor = System.Drawing.Color.White;
-            this.secondLabel.Location = new System.Drawing.Point(401, 93);
+            this.secondLabel.Location = new System.Drawing.Point(402, 48);
             this.secondLabel.Name = "secondLabel";
             this.secondLabel.Size = new System.Drawing.Size(111, 94);
             this.secondLabel.TabIndex = 3;
@@ -57,11 +58,21 @@
             // 
             this.dateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateLabel.ForeColor = System.Drawing.Color.White;
-            this.dateLabel.Location = new System.Drawing.Point(118, 187);
+            this.dateLabel.Location = new System.Drawing.Point(109, 142);
             this.dateLabel.Name = "dateLabel";
             this.dateLabel.Size = new System.Drawing.Size(449, 94);
             this.dateLabel.TabIndex = 4;
             this.dateLabel.Text = "Jan 03 2017";
+            // 
+            // weekDayLabel
+            // 
+            this.weekDayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weekDayLabel.ForeColor = System.Drawing.Color.White;
+            this.weekDayLabel.Location = new System.Drawing.Point(127, 236);
+            this.weekDayLabel.Name = "weekDayLabel";
+            this.weekDayLabel.Size = new System.Drawing.Size(449, 94);
+            this.weekDayLabel.TabIndex = 5;
+            this.weekDayLabel.Text = "Tuesday";
             // 
             // Form1
             // 
@@ -69,6 +80,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(795, 383);
+            this.Controls.Add(this.weekDayLabel);
             this.Controls.Add(this.dateLabel);
             this.Controls.Add(this.secondLabel);
             this.Controls.Add(this.hourMinuteLabel);
@@ -83,6 +95,7 @@
         private System.Windows.Forms.Label hourMinuteLabel;
         private System.Windows.Forms.Label secondLabel;
         private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label weekDayLabel;
     }
 }
 
